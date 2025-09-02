24c78500982301f906da5aab79baeffb16abcaf3
Luis Estevam <luesrebonat@gmail.com>
2025-09-02 17:39:29 -0300

    Meio de turno 02/09

--- app/src/main/java/com/luisestevam/kanbantask/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/luisestevam/kanbantask/ui/auth/LoginFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.luisestevam.kanbantask.databinding.FragmentLoginBinding
+import com.luisestevam.kanbantask.R
 
 class LoginFragment : Fragment() {
 
@@ -20,6 +22,22 @@ class LoginFragment : Fragment() {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.btnRecover.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()

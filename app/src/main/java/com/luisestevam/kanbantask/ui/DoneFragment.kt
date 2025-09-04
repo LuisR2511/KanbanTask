5cf71878b0269971a029a4b94ed7d6fb7bb3847e
Luis Estevam <luesrebonat@gmail.com>
2025-09-04 19:38:05 -0300

    Parte5 04/09

--- app/src/main/java/com/luisestevam/kanbantask/ui/DoneFragment.kt
+++ app/src/main/java/com/luisestevam/kanbantask/ui/DoneFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.luisestevam.kanbantask.R
 import com.luisestevam.kanbantask.data.model.Status
@@ -29,15 +30,32 @@ class DoneFragment : Fragment() {
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        initRecyclerView(getTask())
-    }
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView(getTask())    }
     private fun initRecyclerView(taskList: List<Task>) {
-        taskAdapter = TaskAdapter(requireContext(), taskList)
+        taskAdapter = TaskAdapter(requireContext(), taskList) { task, option -> optionSelected(task, option) }
         binding.recyclerViewTask.layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerViewTask.setHasFixedSize(true)
 
         binding.recyclerViewTask.adapter = taskAdapter
     }
+    private fun optionSelected(task: Task, option: Int) {
+        when (option) {
+            TaskAdapter.SELECT_REMOVER -> {
+                Toast.makeText(requireContext(), "Removendo ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_EDIT -> {
+                Toast.makeText(requireContext(), "Editando ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_DETAILS -> {
+                Toast.makeText(requireContext(), "Detalhes ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_BACK -> {
+                Toast.makeText(requireContext(), "Voltando para Fazendo", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     private fun getTask() = listOf(
         Task("12", "Configurar tema principal do app", Status.DONE),
         Task("13", "Criar tela de splash", Status.DONE),

73e218c221fca7b839b4eaef8d01c8b1212e6f8a
LuisR2511 <luesrebonat@gmail.com>
2025-10-10 09:01:33 -0300

    Firebase

--- app/src/main/java/com/luisestevam/kanbantask/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/luisestevam/kanbantask/ui/auth/RegisterFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.luisestevam.kanbantask.R
 import com.luisestevam.kanbantask.databinding.FragmentRegisterBinding
 import com.luisestevam.kanbantask.util.initToolbar
@@ -15,7 +16,7 @@ class RegisterFragment : Fragment() {
 
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
